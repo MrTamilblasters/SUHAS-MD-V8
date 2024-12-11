396b0c26bbe9c8e21cb6186caac1942de6943812
MrTamilblasters <mrhumais4@gmail.com>
2024-12-11 09:48:40 +0530

    💕SUHAS-MD 🧚‍♂️

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const { File } = require('megajs')
 const prefix = '.'
 
 
-const ownerNumber = ['94729586266']
+const ownerNumber = ['94762454869']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
